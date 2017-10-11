b403f9c093c34e212c3633b2cbc01368ce03c341
MACHALITE\Machalite <jonathanhuang7070@gmail.com>
2017-10-11 17:27:06 +0700

    changed connection for heroku deployment

--- connection.php
+++ connection.php
@@ -1,28 +1,38 @@
 <?php
+$url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+
+$server = $url["host"];
+$username = $url["user"];
+$password = $url["pass"];
+$db = substr($url["path"], 1);
+
+$conn = new mysqli($server, $username, $password, $db);
+
+if (mysqli_connect_errno())
+{
+	// if connection error, display this
+	echo $connectionDbConFail . mysqli_connect_error();
+}
+
+
 	//settings for DB connection
 
 	//fetch strings to display
-	include_once("strings.php");
+	// include_once("strings.php");
 
 	//declaration and initialisation
-	$host="us-cdbr-iron-east-05.cleardb.net"; //localhost if hosted locally
-	$user="bfc1e725797bdf"; //default=root
-	$pass="c4e344fc";
-	$db="heroku_2ad6e3d7ef27f71"; //database name (default=uttadb)
-
-	//test variable
-	// $host="120.89.92.80"; //localhost if hosted locally
-	// $user="atmajaya_utta"; //default=root
-	// $pass="=+6gPpL+GUfK";
-	// $db="atmajaya_uttadb"; //database name (default=uttadb)
-
-	//connect to database
-	$con=mysqli_connect($host,$user,$pass,$db);
-
-	// Check connection
-	if (mysqli_connect_errno())
-	{
-		// if connection error, display this
-		echo $connectionDbConFail . mysqli_connect_error();
-	}
+	// $host="us-cdbr-iron-east-05.cleardb.net"; //localhost if hosted locally
+	// $user="bfc1e725797bdf"; //default=root
+	// $pass="c4e344fc";
+	// $db="heroku_2ad6e3d7ef27f71"; //database name (default=uttadb)
+	//
+	// //connect to database
+	// $con=mysqli_connect($host,$user,$pass,$db);
+	//
+	// // Check connection
+	// if (mysqli_connect_errno())
+	// {
+	// 	// if connection error, display this
+	// 	echo $connectionDbConFail . mysqli_connect_error();
+	// }
 ?>
