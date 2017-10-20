0f84754ee861c9e6ba9d58f2a0c3eb9a83c1c841
MACHALITE\Machalite <jonathanhuang7070@gmail.com>
2017-10-21 00:07:04 +0700

    changed table usagelog

--- connection.php
+++ connection.php
@@ -19,26 +19,4 @@ if (mysqli_connect_errno())
 	// if connection error, display this
 	echo $connectionDbConFail . mysqli_connect_error();
 }
-
-
-	//settings for DB connection
-
-	//fetch strings to display
-	// include_once("strings.php");
-
-	//declaration and initialisation
-	// $host="us-cdbr-iron-east-05.cleardb.net"; //localhost if hosted locally
-	// $user="bfc1e725797bdf"; //default=root
-	// $pass="c4e344fc";
-	// $db="heroku_2ad6e3d7ef27f71"; //database name (default=uttadb)
-	//
-	// //connect to database
-	// $con=mysqli_connect($host,$user,$pass,$db);
-	//
-	// // Check connection
-	// if (mysqli_connect_errno())
-	// {
-	// 	// if connection error, display this
-	// 	echo $connectionDbConFail . mysqli_connect_error();
-	// }
 ?>
