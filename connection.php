14f25c8763338cd502172754e3cabe356b57bdb3
machalite <machalite@ymail.com>
2018-02-01 00:43:09 +0700

    Update connection.php

--- connection.php
+++ connection.php
@@ -1,10 +1,10 @@
 <?php
 include_once("strings.php");
 
-$server = "us-cdbr-iron-east-05.cleardb.net";
-$username = "bfc1e725797bdf";
-$password = "c4e344fc";
-$db = "heroku_2ad6e3d7ef27f71";
+$server = "";
+$username = "";
+$password = "";
+$db = "";
 
 // $server = "localhost";
 // $username = "root";
